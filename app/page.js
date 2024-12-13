913bd4691c4ecdc69ff15a618dc8add1302d6847
0gyo <ken107island@gmail.com>
2024-12-14 02:00:51 +0900

    이미지 파일 png로 변경

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
-import section_1 from '../app/assets/section_1.optimized.svg';
-import section_2 from '../app/assets/section_2.optimized.svg';
-import section_3 from '../app/assets/section_3.optimized.svg';
-import section_4 from '../app/assets/section_4.optimized.svg';
-import section_5 from '../app/assets/section_5.optimized.svg';
-import section_6 from '../app/assets/section_6.optimized.svg';
+import section_1 from '../app/assets/section_1.png';
+import section_2 from '../app/assets/section_2.png';
+import section_3 from '../app/assets/section_3.png';
+import section_4 from '../app/assets/section_4.png';
+import section_5 from '../app/assets/section_5.png';
+import section_6 from '../app/assets/section_6.png';
+import section_7 from '../app/assets/section_7.png';
 
 export default function Home() {
   return (
@@ -77,6 +78,19 @@ export default function Home() {
         <img
           src={section_6.src}
           alt="Homepage Section 6"
+          style={{
+            width: '100%',
+            height: 'auto',
+            display: 'block',
+            margin: 0,
+            padding: 0
+          }}
+        />
+      </div>
+      <div className="image-container">
+        <img
+          src={section_7.src}
+          alt="Homepage Section 7"
           style={{
             width: '100%',
             height: 'auto',
