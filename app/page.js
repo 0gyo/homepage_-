0c8418431e97d31c33ad3fad81e4f0eb26de33cb
0gyo <ken107island@gmail.com>
2024-12-13 23:00:06 +0900

    svg 처리 방식 수정

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import section_3 from '../app/assets/section_3.svg';
 import section_4 from '../app/assets/section_4.svg';
 import section_5 from '../app/assets/section_5.svg';
 import section_6 from '../app/assets/section_6.svg';
+
 export default function Home() {
   return (
     <main style={{ margin: 0, padding: '0 0 20px 0', width: '100%' }}>
@@ -20,14 +21,17 @@ export default function Home() {
             height: 'auto',
             display: 'block',
             margin: 0,
-            padding: 0
+            padding: 0,
+            maxWidth: '100vw'
           }}
+          sizes="100vw"
         />
       </div>
       <div className="image-container">
         <Image
           src={section_2}
           alt="Homepage Section 2"
+          priority
           width={1920}
           height={1080}
           style={{
@@ -35,14 +39,17 @@ export default function Home() {
             height: 'auto',
             display: 'block',
             margin: 0,
-            padding: 0
+            padding: 0,
+            maxWidth: '100vw'
           }}
+          sizes="100vw"
         />
       </div>
       <div className="image-container">
         <Image
           src={section_3}
           alt="Homepage Section 3"
+          priority
           width={1920}
           height={1080}
           style={{
@@ -50,14 +57,17 @@ export default function Home() {
             height: 'auto',
             display: 'block',
             margin: 0,
-            padding: 0
+            padding: 0,
+            maxWidth: '100vw'
           }}
+          sizes="100vw"
         />
       </div>
       <div className="image-container">
         <Image
           src={section_4}
           alt="Homepage Section 4"
+          priority
           width={1920}
           height={1080}
           style={{
@@ -65,14 +75,17 @@ export default function Home() {
             height: 'auto',
             display: 'block',
             margin: 0,
-            padding: 0
+            padding: 0,
+            maxWidth: '100vw'
           }}
+          sizes="100vw"
         />
       </div>
       <div className="image-container">
         <Image
           src={section_5}
           alt="Homepage Section 5"
+          priority
           width={1920}
           height={1080}
           style={{
@@ -80,14 +93,17 @@ export default function Home() {
             height: 'auto',
             display: 'block',
             margin: 0,
-            padding: 0
+            padding: 0,
+            maxWidth: '100vw'
           }}
+          sizes="100vw"
         />
       </div>
       <div className="image-container">
         <Image
           src={section_6}
           alt="Homepage Section 6"
+          priority
           width={1920}
           height={1080}
           style={{
@@ -95,8 +111,10 @@ export default function Home() {
             height: 'auto',
             display: 'block',
             margin: 0,
-            padding: 0
+            padding: 0,
+            maxWidth: '100vw'
           }}
+          sizes="100vw"
         />
       </div>
     </main>
